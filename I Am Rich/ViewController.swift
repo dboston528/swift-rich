2854481e768aacde02568e74638ea7dedce59774
DeAndre Boston <dboston3@yahoo.com>
2020-01-30 23:07:12 -0600

    Initial commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by DeAndre Boston on 1/27/20.
+//  Copyright © 2020 DeAndre Boston. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
